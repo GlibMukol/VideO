5d45a6b68fa5b8c75642492a1c5f5ee78332be00
Mykola <gray.grifon@gmail.com>
2022-01-12 17:40:45 +0200

    implement to local MySql

--- server/app/index.ts
+++ server/app/index.ts
@@ -4,7 +4,10 @@ import Router from './routers';
 
 const app = express();
 const port = process.env.PORT || 8000;
+
+console.log(`port`, port)
+
 app.use('/', Router);
 app.listen(port, ()  => {
-  return console.log(`server is listening on ${port}`);
+  return console.log(`server is listening on 123 ${port}`);
 });
